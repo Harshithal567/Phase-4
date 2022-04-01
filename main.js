5931d141bd60be98acc165e37f556e19c9c3fd69
Harshithal567 <harshithalgowda24@gmail.com>
2022-04-01 21:47:04 +0530

    projects added of phase 4

---
+++ main.js
@@ -0,0 +1,40 @@
+function  test() {
+
+var x = 99; 
+
+console.log("X value is : "+x);
+
+x = "rahul kumar";
+
+console.log(x);
+
+x= true;
+console.log(x);
+
+} 
+
+function add(a,b) { 
+result = 0;
+console.log(result)
+    return a+b;
+} 
+ //var result = add(5,7);
+
+ //console.log(result) 
+
+ function newKeywords() {
+
+let name = "javeed";
+console.log(name)
+const amount = 80000; 
+
+console.log(amount)
+if(true) {
+
+let city = "hyderabad";
+console.log(city);
+
+}
+}
+ 
+console.log(this.name)
